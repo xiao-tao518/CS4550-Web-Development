b6e49fa20f1a406ee9b69200f2fded6db9a21350
Xiao Tao <61165887+xiao-tao518@users.noreply.github.com>
2021-02-13 01:03:36 -0500

    Add files via upload

---
+++ hw04/test/practice_web/controllers/page_controller_test.exs
@@ -0,0 +1,28 @@
+defmodule PracticeWeb.PageControllerTest do
+  use PracticeWeb.ConnCase
+
+  test "GET /", %{conn: conn} do
+    conn = get conn, "/"
+    assert html_response(conn, 200) =~ "Factor"
+  end
+
+  test "double 5", %{conn: conn} do
+    conn = post conn, "/double", %{"x" => "5"}
+    assert html_response(conn, 200) =~ "10"
+  end
+
+  test "calc 3 * 5 - 10 / 2", %{conn: conn} do
+    conn = post conn, "/calc", %{"expr" => "3 * 5 - 10 / 2"}
+    assert html_response(conn, 200) =~ "10"
+  end
+
+  test "factor 255", %{conn: conn} do
+    conn = post conn, "/factor", %{"x" => "255"}
+    assert html_response(conn, 200) =~ "17"
+  end
+
+  test "palindrome? qwewq", %{conn: conn} do
+    conn = post conn, "/palindrome", %{"x" => "qwewq"}
+    assert html_response(conn, 200) =~ "True, it is a palindrome"
+  end
+end
